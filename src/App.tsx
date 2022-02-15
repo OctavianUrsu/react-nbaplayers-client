ba8bb747b07a816944ede30ebf237d7c4a272b72
OctavianUrsu <phenomenno@gmail.com>
2022-02-15 22:58:37 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
+interface Player {
+  playerId: string;
+  firstName: string;
+  lastName: string;
+  teamId: number;
+}
+
+const BASE_URL =
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BASE_URL : '';
+
+const usePlayers = (): Player[] => {
+  const [players, setPlayers] = useState<Player[]>([]);
+
+  const getPlayers = async () => {
+    const res = await fetch(`${BASE_URL}/api/players/`);
+    const data = await res.json();
+    setPlayers(data);
+  };
+
+  useEffect(() => {
+    getPlayers();
+  }, []);
+
+  return players;
+};
+
 function App() {
+  const players = usePlayers();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div>
+          <h2>🏀 NBA Players</h2>
+        </div>
+        <div className='Search-box'>
+          <Autocomplete
+            options={players}
+            sx={{ width: 700 }}
+            renderInput={(params) => (
+              <TextField {...params} label='Search players' />
+            )}
+            getOptionLabel={(option) =>
+              option.firstName + ' ' + option.lastName
+            }
+          />
+        </div>
       </header>
     </div>
   );
