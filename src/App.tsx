df2011e76970d93db294370a57f6f003c94ad5de
OctavianUrsu <phenomenno@gmail.com>
2022-02-18 19:15:46 +0200

    Fix search bar + add query params

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+import { debounce } from 'lodash';
 
 interface Player {
   playerId: string;
@@ -14,24 +21,127 @@ interface Player {
 const BASE_URL =
   process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BASE_URL : '';
 
-const usePlayers = (): Player[] => {
+function App() {
+  //
+  const [inputValue, setInputValue] = useState<string>('');
   const [players, setPlayers] = useState<Player[]>([]);
+  const [player, setPlayer] = useState<Player | null>(null);
 
-  const getPlayers = async () => {
-    const res = await fetch(`${BASE_URL}/api/players/`);
-    const data = await res.json();
-    setPlayers(data);
+  //
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Debounced search and call to API
+  const debouncedSearch = React.useRef(
+    debounce(async (inputValue: string) => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `${BASE_URL}/api/players/search?name=${inputValue}`
+        );
+        const players = await response.json();
+
+        console.log('calling api for ', inputValue);
+
+        setPlayers(players);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+      }
+    }, 500)
+  ).current;
+
+  // Call the search function on input change
+  const onInputChangeHandler = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLInputElement;
+
+    if (target.value === undefined || target.value === null) {
+      target.value = '';
+    } else {
+      setInputValue(target.value);
+
+      if (target.value.length > 2) {
+        setOpen(true);
+        debouncedSearch(target.value);
+        console.log('calling debouncer for', target.value);
+      }
+    }
   };
 
-  useEffect(() => {
-    getPlayers();
-  }, []);
+  // Handle the onClose event
+  const handleOnClose = () => {
+    setOpen(false);
+    setPlayers([]);
+    setInputValue('');
+  };
 
-  return players;
-};
+  const searchBox = (
+    <Autocomplete
+      id='player-search'
+      filterOptions={(x) => x}
+      open={open}
+      onClose={handleOnClose}
+      getOptionLabel={(player) => player.firstName + ' ' + player.lastName}
+      options={players}
+      isOptionEqualToValue={(o, v) => o.playerId === v.playerId}
+      loading={isLoading}
+      defaultValue={null}
+      value={player}
+      onChange={(_, value) => setPlayer(value)}
+      onInputChange={onInputChangeHandler}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          type='string'
+          label='Search players'
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {isLoading ? (
+                  <CircularProgress color='inherit' size={16} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
 
-function App() {
-  const players = usePlayers();
+  const PlayerCard = (
+    <Card>
+      <CardContent>
+        <div className='Player-label'>
+          <div>
+            <label>
+              <Typography variant='overline'>ID:</Typography>
+            </label>
+            <Typography variant='body1'>{player?.playerId}</Typography>
+          </div>
+          <div>
+            <label>
+              <Typography variant='overline'>First Name:</Typography>
+            </label>
+            <Typography variant='body1'>{player?.firstName}</Typography>
+          </div>
+          <div>
+            <label>
+              <Typography variant='overline'>Last Name:</Typography>
+            </label>
+            <Typography variant='body1'>{player?.lastName}</Typography>
+          </div>
+          <div>
+            <label>
+              <Typography variant='overline'>Team:</Typography>
+            </label>
+            <Typography variant='body1'>{player?.teamId}</Typography>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
 
   return (
     <div className='App'>
@@ -39,18 +149,8 @@ function App() {
         <div>
           <h2>🏀 NBA Players</h2>
         </div>
-        <div className='Search-box'>
-          <Autocomplete
-            options={players}
-            sx={{ width: 700 }}
-            renderInput={(params) => (
-              <TextField {...params} label='Search players' />
-            )}
-            getOptionLabel={(option) =>
-              option.firstName + ' ' + option.lastName
-            }
-          />
-        </div>
+        <div className='Search-box'>{searchBox}</div>
+        {player && <div className='Player-card'>{PlayerCard}</div>}
       </header>
     </div>
   );
